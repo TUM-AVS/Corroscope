f3acb0d985cb4bcf14eccc8f544699ca026d7eec
Tobias Markus <tobias.markus@tum.de>
2023-08-05 17:49:46 +0200

    build.rs: Rebuild conditionally

--- build.rs
+++ build.rs
@@ -1,8 +1,13 @@
 extern crate prost_build;
 
 fn main() {
+    const PROTO_DIR: &'static str = "src/commonroad_pb";
+    const PROTO_FILE: &'static str = "src/commonroad_pb/commonroad.proto";
+
+    println!("cargo:rerun-if-changed={}", PROTO_DIR);
+
     prost_build::compile_protos(
-        &["src/commonroad_pb/commonroad.proto"],
+        &[PROTO_FILE],
         &["src/commonroad_pb"],
     )
     .unwrap();
